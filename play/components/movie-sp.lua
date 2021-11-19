22efd98eec68c67e0336e444745edfb05fc04282
Nathan Du <nathandu@outlook.com>
2021-11-19 16:53:56 +0800

    remove op from bga component
    can cause issues sometimes

--- play/components/movie-sp.lua
+++ play/components/movie-sp.lua
@@ -34,7 +34,6 @@ return function(centerX, centerY)
             },
             {
                 id = "movie",
-                op = {41},
                 timer = 41,
                 blend = 2,
                 dst = {
