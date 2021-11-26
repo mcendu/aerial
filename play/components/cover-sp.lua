1d2324bb3e4f3f77f391ec96aa04cf737593d35c
Nathan Du <nathandu@outlook.com>
2021-11-26 11:28:02 +0800

    add cover amount indicators

--- play/components/cover-sp.lua
+++ play/components/cover-sp.lua
@@ -17,27 +17,67 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]
 
-return function(anchorX)
+local Component = require("ash.skin")
+
+local function coverValue(name, centerX, anchorY, op, offsets)
     return {
+        destination = {
+            {
+                id = name,
+                loop = 1200,
+                op = op,
+                offsets = offsets,
+                dst = {
+                    { time = 1000, x = centerX - 90, y = anchorY, w = 15, h = 20, a = 0 },
+                    { time = 1200, a = 255 }
+                }
+            },
+            {
+                id = "green-number",
+                loop = 1200,
+                op = op,
+                offsets = offsets,
+                dst = {
+                    { time = 1000, x = centerX + 45, y = anchorY, w = 15, h = 20,
+                        r = 0x93, g = 0xd1, b = 0x98, a = 0 },
+                    { time = 1200, a = 255 }
+                }
+            }
+        }
+    }
+end
+
+return function(anchorX, anchorY)
+    anchorY = anchorY or 370
+
+    local height = 1080 - anchorY
+
+    local c = Component:new {
         source = {
             { id = "cover", path = "textures/lanecover/*.png" },
             { id = "lift", path = "textures/lift/*.png"}
         },
         slider = {
-            { id = "lanecover", src = "cover", x = 0, y = 0, w = 460, h = 710, angle = 2, range = 710, type = 4 }
+            { id = "lanecover", src = "cover", x = 0, y = 0, w = -1, h = -1, angle = 2, range = 710, type = 4 }
         },
         liftCover = {
-            { id = "lift", src = "lift", x = 0, y = 0, w = 460, h = 710, disapearLine = 370 }
+            { id = "lift", src = "lift", x = 0, y = 0, w = -1, h = -1, disapearLine = 370 }
         },
         hiddenCover = {
             { id = "hiddencover", src = "lift", x = 0, y = 0, w = 460, h = 710, disapearLine = 370 }
+        },
+        value = {
+            { id = "cover-amount", src = 1, x = 0, y = 128, w = 165, h = 20, divx = 11, digit = 3, ref = 14 },
+            { id = "lift-amount", src = 1, x = 0, y = 128, w = 165, h = 20, divx = 11, digit = 3, ref = 314 },
+            { id = "hidden-amount", src = 1, x = 0, y = 128, w = 165, h = 20, divx = 11, digit = 3, ref = 315 },
+            { id = "green-number", src = 1, x = 0, y = 128, w = 165, h = 20, divx = 11, digit = 3, ref = 313 },
         },
         destination = {
             {
                 id = "lanecover",
                 loop = 1000,
                 dst = {
-                    { time = 500, x = anchorX, y = 1080 + 710, w = 460, h = 710, acc = 2 },
+                    { time = 500, x = anchorX, y = 1080 + height, w = 460, h = height, acc = 2 },
                     { time = 1000, y = 1080 }
                 }
             },
@@ -45,18 +85,24 @@ return function(anchorX)
                 id = "lift",
                 loop = 1000,
                 dst = {
-                    { time = 500, x = anchorX, y = 370 - 710 * 2, w = 460, h = 710, acc = 2 },
-                    { time = 1000, y = 370 - 710 }
+                    { time = 500, x = anchorX, y = anchorY - height * 2, w = 460, h = height, acc = 2 },
+                    { time = 1000, y = anchorY - height }
                 }
             },
             {
                 id = "hiddencover",
                 loop = 1000,
                 dst = {
-                    { time = 500, x = anchorX, y = 370 - 710 * 2, w = 460, h = 710, acc = 2 },
-                    { time = 1000, y = 370 - 710 }
+                    { time = 500, x = anchorX, y = anchorY - height * 2, w = 460, h = height, acc = 2 },
+                    { time = 1000, y = anchorY - height }
                 }
             }
         }
     }
+
+    c:addComponent(coverValue("cover-amount", anchorX + 230, 1090, {270, 271}, {4}))
+    c:addComponent(coverValue("lift-amount", anchorX + 230, anchorY - 30, {270, 272}, {3}))
+    c:addComponent(coverValue("hidden-amount", anchorX + 230, anchorY - 30, {270, 273}, {5}))
+
+    return c
 end
